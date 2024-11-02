ae4a4107b3821201a26f2c7c4eef53ed6f058c88
Demian Parkhomenko <95881717+DemianParkhomenko@users.noreply.github.com>
2024-11-02 12:11:43 +0200

    Fix relation between channel and application

--- JavaScript/8-delegation.js
+++ JavaScript/8-delegation.js
@@ -6,11 +6,11 @@ class Channel {
   constructor(application, name) {
     this.application = application;
     this.name = name;
-    this.histry = [];
+    this.history = [];
   }
 
   send(from, message) {
-    this.histry.push({ from, message });
+    this.history.push({ from, message });
     this.application.send(this.name, from, message);
   }
 }
@@ -21,8 +21,8 @@ class Application {
     this.channels = {};
   }
 
-  send(channel, from, message) {
-    this.events.emit('message', this.name, from, message);
+  send(channelName, from, message) {
+    this.events.emit('message', channelName, from, message);
   }
 
   createChannel(name) {
@@ -41,8 +41,8 @@ chat.events.on('channel', (name) => {
   console.log(`Channel ${name} created`);
 });
 
-chat.events.on('message', (channel, from, message) => {
-  console.log(`${from}@${channel}> ${message}`);
+chat.events.on('message', (channelName, from, message) => {
+  console.log(`${from}@${channelName}> ${message}`);
 });
 
 const channel = chat.createChannel('HowProgrammingWorks');
